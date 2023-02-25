2557d227e4ac972e48526faf4b6c952e07387f53
rbzf <anna.sadkowska@gmail.com>
2023-02-25 20:26:57 +0100

    Rozwiazanie poprawione

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -7,22 +7,23 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int input;
+        boolean check;
         do {
             System.out.println("Podaj liczbe");
             input = scanner.nextInt();
             if (input <= 100) {
                 System.out.println("Podana liczba jest za mala.");
+                check = false;
             } else if (input >= 200) {
                 System.out.println("Podana liczba jest za duza.");
+                check = false;
             } else if (input % 3 != 0) {
                 System.out.println("Podana liczba nie jest podzielna przez 3.");
+                check = false;
             } else {
                 System.out.println("Podana liczba jest ok");
+                check = true;
             }
-
-        } while (!((input > 100) && (input < 200) && (input % 3 == 0)));
-
-        scanner.close();
-
+        } while (!check);
     }
 }
